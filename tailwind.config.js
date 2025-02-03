2ffea2812e970aa6f4f5119256e825820d99ecad
tanvirabsar <tjoy4799@gmail.com>
2025-02-03 12:15:29 +0600

    3rd commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // other configurations...
+    daisyui: {
+      themes: false, // This will disable all themes except the default light theme
+      // or you can specify only the light theme
+      // themes: ["light"],
+    },
+  };
+  
